cbe28e409d23238118a01d53c41961671dde4caf
fcoudin <xltri>
2021-06-28 16:03:57 +0200

    new commit after reload .git origin

--- src/component/DoctorList/DoctorListElement.tsx
+++ src/component/DoctorList/DoctorListElement.tsx
@@ -1,20 +1,85 @@
-// map of the list of doctors for getDataApi/DoctorsList
+// map of the list of doctors for index
 import React from "react";
-import { TDoctorListProps, DoctorInfo } from "./index";
+import styled from "styled-components";
+import { TDoctorListProps } from "./index";
+import DoctorAvailabilities from "./DoctorAvailabilities";
 
+
+const Container = styled.ul`
+  diplay: flex;
+  margin-top: 5rem;
+`;
+
+const Row = styled.div`
+  &::after {
+    content: "";
+    clear: both;
+  }
+`;
+
+export interface Span {
+  span: string;
+}
+
+const Column = styled.div`
+  display: table;
+  float: left;
+  width: 100%;
+  @media only screen and (min-width: 1024px) {
+    ${(props: Span) => ``};
+    width: ${(props?: any) => (props.span ? (props.span / 12) * 100 : "8.33")}%;
+  } ;
+`;
+
+const Box = styled.li`
+  display: flex;
+  flex-wrap: wrap;
+  font-size: 1.1rem;
+  width: 60%;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 1rem;
+  padding: 0.5rem;
+  border: 3px solid silver;
+  border-radius: 3px;
+  list-style: none;
+  @media only screen and (max-width: 768px) {
+    width: 100%;
+    font-size: 1rem;
+  } ;
+`;
 
 const Doctors: React.FC<TDoctorListProps> = ({ items }) => {
-  console.log("props", items && items);
   return (
     <div>
-      <ul>
+      <Container>
         {items &&
-          items.map((item: DoctorInfo) => (
-            <li key={item.id}>
-              <span>{item.name}</span>
-            </li>
+          items.map((item: any) => (
+            <Box key={item.id}>
+              <Row>
+                <Column span="4">
+                  <h3>{item.name}</h3>
+                  <p>
+                    {item.address.city} ({item.address.country})
+                  </p>
+                  
+                  <p></p>
+                  <br />
+                </Column>
+                <Column span="6">
+                  <span>
+                    <DoctorAvailabilities test={item.id} name={item.name}/>
+                  </span>
+                  <br />
+                </Column>
+                <Column span="2">
+                  <p>Book an appointment with:</p><b>{item.name}</b>
+                  <br />
+                </Column>
+              </Row>
+            </Box>
           ))}
-      </ul>
+      </Container>
     </div>
   );
 };
--- src/component/DoctorList/index.tsx
+++ src/component/DoctorList/index.tsx
@@ -1,6 +1,7 @@
-// display list of doctors
+// display list of doctors with DoctorListElement  component
 import React, { useEffect, useState, Fragment } from "react";
 import Doctors from "./DoctorListElement";
+import Doc from "./DocElement";
 import axios, { AxiosResponse } from "axios";
 
 // contact description for each doctor
@@ -8,33 +9,40 @@ export interface DoctorInfo {
   id?: string;
   name?: string;
   address?: {
-    city: string;
-    country: string;
-    line1: string;
-    line2: string;
-    postalCode: number;
-
+    city?: string;
+    country?: string;
+    line1?: string;
+    line2?: string;
+    postalCode?: number;
   };
 }
 // List of doctors to use as props type
 // export type TDoctorList = DoctorInfo[];
 export interface TDoctorListProps {
-  items?: any;
+  items?: DoctorInfo[];
 }
 
-const DoctorList: React.FC<TDoctorListProps> = () => {
-  const [doctor, setDoctorList] = useState<TDoctorListProps>([] as any);
+const DoctorList: React.FC<any> = (doctorId: any) => {
+  const [doctor, setDoctorList] = useState<any>([]);
 
   useEffect(() => {
     axios
       .get("https://tech-test.joovence.dev/api/doctors")
       .then((response: AxiosResponse) => {
-        console.log(response.data);
         setDoctorList(response.data);
       });
   }, []);
 
-  console.log("doctors...", doctor);
+  if (doctorId.item) {
+    const r = doctor.filter((e: any) => e.name === doctorId.item);
+
+    return (
+      <Fragment>
+        <Doc item={r} />
+      </Fragment>
+    );
+  }
+
   return (
     <Fragment>
       <Doctors items={doctor} />
