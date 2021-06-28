d9b6872e868ebd86fe708559e97bb0daa50343a8
fc_myunderscore <45292917+Myunderscore@users.noreply.github.com>
2021-06-28 16:17:13 +0200

    Delete App.test.tsx

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react'
-import { shallow } from 'enzyme'
-import App from './App'
-
-test('renders the heading', () => {
-  const result = shallow(<App />).contains(<h1>Your dashboard for energy </h1>)
-  expect(result).toBeTruthy()
-})
